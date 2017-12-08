7cc30b9383ecf8f8935e17f4245aaeabc7e1d579
Nathan White <najowhit@iupui.edu>
2017-12-08 13:37:07 -0500

    fixed a few typos, fixed  issue with transpose

--- src/main.py
+++ src/main.py
@@ -1,54 +1,17 @@
 from openpyxl import load_workbook
 from CSR import *
+from ContextRecommender import *
 import os.path
 
 
-# getUsers returns a list of the (unique) users
-def getUsers(ws):
-    user_list = []
-
-    user_column = ws['A']
-    for cell in range(2, len(user_column)):
-        if user_column[cell].value not in user_list:
-            user_list.append(user_column[cell].value)
-
-    return user_list
-
 
 def main():
-    wb = load_workbook(os.path.dirname(__file__) + '../Data_InCarMusic.xlsx')
-    ws = wb["TrainSet"]
 
-    user_list = getUsers(ws)
-
-    special_csr = CSR()
-    driving_style_csr = CSR()
-    landscape_csr = CSR()
-    mood_csr = CSR()
-    natural_csr = CSR()
-    road_type_csr = CSR()
-    sleepiness_csr = CSR()
-    traffic_cond_csr = CSR()
-    weather_csr = CSR()
-
-    # CSR for songs rated with no context data
-    special_csr.build_no_context(ws, user_list)
-
-    driving_style_csr.build_from_excel(ws, user_list, dimension=4)
-    landscape_csr.build_from_excel(ws, user_list, dimension=5)
-    mood_csr.build_from_excel(ws, user_list, dimension=6)
-    natural_csr.build_from_excel(ws, user_list, dimension=7)
-    road_type_csr.build_from_excel(ws, user_list, dimension=8)
-    sleepiness_csr.build_from_excel(ws, user_list, dimension=9)
-    traffic_cond_csr.build_from_excel(ws, user_list, dimension=10)
-    weather_csr.build_from_excel(ws, user_list, dimension=11)
-
-    ds_sim = driving_style_csr.calculate_cosine_sim()
-
-    # Redundantly generates sim matrix again, but nicely outputs data
-    # in the following form: 'i j value'
-    #driving_style_csr.calculate_and_output_cosine_sim('sim.txt', -1)
-
+    recommender = ContextRecommender()
+    recommender.loadWb()
+    recommender.createContextCSR()
+    recommender.recommend()
+    print("Done")
 
 
 main()
