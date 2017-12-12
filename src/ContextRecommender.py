2ba8f4cec8fbf652c73be373595d7e38255f0cdd
Nathan White <najowhit@iupui.edu>
2017-12-11 20:54:22 -0500

    refactored excel based CSRs to CSV based

--- src/ContextRecommender.py
+++ src/ContextRecommender.py
@@ -7,36 +7,32 @@ import numpy as np
 
 class ContextRecommender:
     def __init__(self):
-        self.wb = None
         self.train = None
         self.test = None
         self.contextList = []
 
     def loadWb(self):
+
         # load wb on mac
-        self.wb = load_workbook(os.path.dirname(__file__) + '/Data_InCarMusic.xlsx')
-        #self.train = data_set = np.loadtxt(os.path.dirname(__file__) + '/trainCSV.csv', delimiter=',')
-        #self.test = data_set = np.loadtxt(os.path.dirname(__file__) + '/testCSV.csv', delimiter=',')
+        train_set = np.loadtxt(os.path.dirname(__file__) + '/trainCSV.csv', delimiter=',')
+        test_set = np.loadtxt(os.path.dirname(__file__) + '/testCSV.csv', delimiter=',')
 
         # load wb on windows
-        #self.wb = load_workbook(os.path.dirname(__file__) + '\\Data_InCarMusic.xlsx')
-        # self.train = data_set = np.loadtxt(os.path.dirname(__file__) + '\\csvMusic.csv', delimiter=',')
+        #train_set = np.loadtxt(os.path.dirname(__file__) + '\\trainCSV.csv', delimiter=',')
+        #test_set = np.loadtxt(os.path.dirname(__file__) + '\\testCSV.csv', delimiter=',')
 
-        self.train = self.wb["TrainSet"]
-        self.test = self.wb["TestSet"]
+
+        self.train = train_set.astype(int)
+        self.test = test_set.astype(int)
 
     # return the list of unique users
     def getUsers(self):
         user_list = []
 
-        user_column = self.train['A']
-        #col_len = self.train.shape[0]
-        for cell in range(2, len(user_column)):
-        #for cell in range(0, col_len):
-            if user_column[cell].value not in user_list:
-            #if self.train[cell][0] not in user_list:
-                user_list.append(user_column[cell].value)
-                #user_list.append(self.train[cell][0])
+        col_len = self.train.shape[0]
+        for cell in range(0, col_len):
+            if self.train[cell][0] not in user_list:
+                user_list.append(self.train[cell][0])
 
         return user_list
 
@@ -44,14 +40,14 @@ class ContextRecommender:
         user_list = self.getUsers()
 
         # for all the dimension create a CSR and add to List
-        for dim in range(4, 12):
+        for dim in range(3, 11):
             dimCSR = CSR()
-            dimCSR.build_from_excel(self.train, user_list, dim)
+            dimCSR.build_from_numpy(self.train, user_list, dim)
             self.contextList.append(dimCSR)
 
         # create a special CSR where no context was given
         noContextCSR = CSR()
-        noContextCSR.build_no_context(self.train, user_list)
+        noContextCSR.build_no_context_numpy(self.train, user_list)
         self.contextList.append(noContextCSR)
 
     def user_item_avg(self, user, item):
@@ -100,7 +96,7 @@ class ContextRecommender:
 
                 # find the change in rating and weight it based on User-User similarity
                 changeInRating = self.user_item_avg(otherUser, item) - transposeDim.context[j]
-                uuSim = self.contextList[dim - 4].calc_user_user_sim(user, otherUser)
+                uuSim = self.contextList[dim - 3].calc_user_user_sim(user, otherUser)
                 cumulativeWeight += (changeInRating * uuSim)
                 numUsers += 1
 
@@ -118,10 +114,10 @@ class ContextRecommender:
         absoluteError = 0.0
         numRating = 0.0
 
-        numRows = len(self.test["A"])
-        for i in range(2, numRows + 1):
-            user = self.test.cell(row=i, column=1).value
-            item = self.test.cell(row=i, column=2).value
+        numRows = self.test.shape[0]
+        for i in range(0, numRows):
+            user = self.test[i][0]
+            item = self.test[i][1]
 
             userBaseline = self.user_item_avg(user, item)
 
@@ -130,7 +126,7 @@ class ContextRecommender:
             # for each dimension that has a value
             for dim in range(0, len(self.contextList) - 1):
 
-                contextValue = self.test.cell(row=i, column=dim + 4).value
+                contextValue = self.test[i][dim + 3]
                 if (contextValue != 0):
                     contextWeight = self.findContextWeight(user, item, dim, contextValue)
 
@@ -155,7 +151,7 @@ class ContextRecommender:
                 contextModifier = overallWeight / numDim
 
             predictRating = userBaseline + contextModifier
-            actualRating = int(self.test.cell(row=i, column=3).value)
+            actualRating = int(self.test[i][2])
             diff = 0
             if (actualRating > predictRating):
                 diff = actualRating - predictRating
